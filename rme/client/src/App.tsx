b8935c08d7242d0b1382220b2e8719d42854f8a6
aoao <nao.iwakura.lain.sheddy@gmail.com>
2021-12-06 23:53:35 +0900

    -

--- rme/client/src/App.tsx
+++ rme/client/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        sadff
+        aaa
         <button onClick={onClickButton}>click</button>
       </header>
     </div>
